2263ea78f34b61fb8ab6f0099e7f0c089e2ca917
BillalbertCode <BillalbertCode@gmail.com>
2024-01-25 22:36:09 -0400

    UI Add Form Notification and input Empresa

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,95 +1,122 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 const FormContact = () => {
     const [state, handleSubmit] = useForm("xayrnnql");
-   /*  const [alert, setalert] = useState(null); */
 
+    useEffect(() => {
+        const forms = document.querySelectorAll('.needs-validation')
+        console.log(forms)
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    }, [])
+    
     if (state.succeeded) {
         return <p>Proceso finalizado</p>;
     }
-    // const handleClick = () => {
-    //     if (state.succeeded) {
-    //         setalert(
-    //             <div className="alert alert-success" role="alert">
-    //                 Mensaje enviado exitosamente! 🥳
-    //             </div>
-    //         );
-    //     } else {
-    //         setalert(
-    //             <div className="alert alert-danger" role="alert">
-    //                 Error al enviar el mensaje 😓 <br />
-    //                 Contacta por otro de los medios.
-    //             </div>
-    //         );
-    //     }
-
-    //     setTimeout(() => {
-    //         setalert(null)
-    //     }, 5000);
-    // }
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <div className="control-group">
+            <form className="needs-validation" onSubmit={handleSubmit} noValidate>
+                <div className="control-group " >
                     <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                        <label htmlFor="name">Nombre o Empresa</label>
+                        <label htmlFor="name">Usuario:</label>
                         <input
                             type="text"
                             className="form-control"
                             id="name"
-                            placeholder="Nombre"
+                            placeholder="Usuario (Nombre)"
                             name="name"
+                            required
                         />
                         <ValidationError
                             prefix="Name"
                             field="name"
                             error={state.errors} />
+                        <div className="invalid-feedback">
+                            Este Campo es requerido.
+                        </div>
+                    </div>
+                    <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                        <label htmlFor="empresa">Empresa:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="empresa"
+                            placeholder="Nombre de la Empresa o Personal"
+                            name="empresa"
+                            required
+                        />
+                        <ValidationError
+                            prefix="Empresa"
+                            field="empresa"
+                            error={state.errors} />
+
+                        <div className="invalid-feedback">
+                            Este Campo es requerido.
+                        </div>
                     </div>
                 </div>
                 <div className="control-group">
                     <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                        <label htmlFor="email">Correo Electrónico</label>
+                        <label htmlFor="email">Correo Electrónico:</label>
                         <input
                             type="email"
                             className="form-control"
                             id="email"
-                            placeholder="Correo Electrónico"
+                            placeholder="Correo Electrónico (name@ejemp.com)"
                             name="email"
+                            required
                         />
                         <ValidationError
                             prefix="Email"
                             field="email"
                             error={state.errors} />
+                        <div className="invalid-feedback">
+                            Ingrese un Correo Electronico valido.
+                        </div>
+                        <div className="valid-feedback">
+                            Se ve bien!
+                        </div>
                     </div>
                 </div>
                 <div className="control-group">
                     <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                        <label htmlFor="message">Mensaje </label>
+                        <label htmlFor="message">Asunto: </label>
                         <textarea
                             className="form-control"
                             id="message"
                             name="message"
                             rows="5"
-                            placeholder="Mensaje // // No olvide enviar informacion clave para estar en contacto"
+                            required
                         />
                         <ValidationError
                             prefix="Message"
                             field="message"
                             error={state.errors} />
+                        <div className="invalid-feedback">
+                            Este Campo es requerido.
+                        </div>
                     </div>
                 </div>
                 <br />
                 <div id="success"></div>
-                <div  className="form-group button-form">
-                    <button  className="btn btn-primary btn-xl" type="submit" disable={state.submitting}/* onClick={handleClick } */ >
+                <div className="form-group button-form">
+                    <button className="btn btn-primary btn-xl" type="submit" disable={state.submitting}>
                         Enviar Mensaje
                     </button>
                 </div>
             </form>
-                   {/*  {alert} */}
         </>
+
     )
 }
 
